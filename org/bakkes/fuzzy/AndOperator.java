f0340d3ad4fdfa599c20e377709be01501119b39
Jappie Klooster <jappieklooster@hotmail.com>
2014-05-31 16:17:53 +0200

    made up the triangle set

--- org/bakkes/fuzzy/AndOperator.java
+++ org/bakkes/fuzzy/AndOperator.java
@@ -2,16 +2,17 @@ package org.bakkes.fuzzy;
 
 public class AndOperator implements IOperator{
 
+	float value = 0;
 	@Override
 	public float operate(IFuzzyTerm with) {
-		// TODO Auto-generated method stub
-		return 0;
+		// 4 and 5 is 9, so and in language is a plus
+		value += with.getValue(); 
+		return value;
 	}
 
 	@Override
 	public float getValue() {
-		// TODO Auto-generated method stub
-		return 0;
+		return value;
 	}
 
 	
