41857f859b62a957e805c2abc264531fa282d09d
Jappie Klooster <jappieklooster@hotmail.com>
2014-05-31 16:17:53 +0200

    made up the triangle set

--- org/bakkes/fuzzy/FuzzySet.java
+++ org/bakkes/fuzzy/FuzzySet.java
@@ -1,11 +1,31 @@
 package org.bakkes.fuzzy;
 
+/**
+ * always a triangle
+ * the max height of the triangle is always one
+ * @author jappie
+ *
+ */
 public class FuzzySet implements IFuzzyTerm{
+
+	float from = 0;
+	float to = 100;
+	/**
+	 * x is the position in the chart where the Y value is retrieved from
+	 */
+	float x = 0;
 
 	@Override
 	public float getValue() {
-		// TODO Auto-generated method stub
-		return 0;
+		float halfDimension = (to - from) /2;
+		if(x < halfDimension){
+			return x / halfDimension;
+		}
+		return (halfDimension - x) / halfDimension;
+	}
+	
+	void setX(float to){
+		x = to;
 	}
 
 }
