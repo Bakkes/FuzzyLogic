201ca79e4e6d964838b87bf5846c0b7fc2139182
U-chris-pc\Chris <chris.m@live.nl>
2014-06-01 18:56:10 +0200

    Created constructors for FuzzyOr and FuzzyAnd

--- org/bakkes/fuzzy/operators/FuzzyAnd.java
+++ org/bakkes/fuzzy/operators/FuzzyAnd.java
@@ -4,6 +4,10 @@ import org.bakkes.fuzzy.IFuzzyTerm;
 
 public class FuzzyAnd extends FuzzyOperator {
 
+	public FuzzyAnd(IFuzzyTerm... terms) {
+		this.operate(terms);
+	}
+	
 	@Override
 	public float getValue() {
 		float smallest = Float.MAX_VALUE;
