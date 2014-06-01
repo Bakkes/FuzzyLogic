361227c01f565459f99815600e1f17d98b074ba2
U-chris-pc\Chris <Chris@chris-pc.(none)>
2014-06-01 15:01:16 +0200

    Added the ability to add a rule

--- org/bakkes/fuzzy/FuzzyRule.java
+++ org/bakkes/fuzzy/FuzzyRule.java
@@ -1,5 +1,12 @@
 package org.bakkes.fuzzy;
 
 public class FuzzyRule {
-
+	
+	private AFuzzyTerm antecedent;
+	private AFuzzyTerm consequence;
+	
+	public FuzzyRule(AFuzzyTerm antecedent, AFuzzyTerm consequence) {
+		this.antecedent = antecedent;
+		this.consequence = consequence;
+	}
 }
