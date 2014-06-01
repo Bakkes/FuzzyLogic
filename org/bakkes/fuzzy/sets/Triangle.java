d1adb43e54b6ddee44fa55e3cb0c7a9b0bc9c1a0
Bakkes <chris.m@live.nl>
2014-06-01 16:51:48 +0200

    Added triangle

---
+++ org/bakkes/fuzzy/sets/Triangle.java
@@ -0,0 +1,37 @@
+package org.bakkes.fuzzy.sets;
+
+import org.bakkes.fuzzy.FloatHelper;
+
+public class Triangle extends FuzzySet {
+
+	private float peakPoint;
+	private float leftOffset;
+	private float rightOffset;
+	
+	public Triangle(float peak, float leftOffset, float rightOffset) {
+		super(peak);
+		this.peakPoint = peak;
+		this.leftOffset = leftOffset;
+		this.rightOffset = rightOffset;
+	}
+
+	@Override
+	public float calculateValue(float val) {
+		if((FloatHelper.areEqual(leftOffset, 0.0f) &&
+				FloatHelper.areEqual(peakPoint, val)) ||
+				(FloatHelper.areEqual(rightOffset, 0.0f) &&
+				FloatHelper.areEqual(peakPoint, val))) {
+			return 1.0f;
+		} else if(val <= peakPoint && val >= (peakPoint - leftOffset)) {
+			float grad = 1.0f / leftOffset;
+			return grad * (val - (peakPoint - leftOffset));
+		} else if(val > peakPoint && val < (peakPoint + rightOffset)) {
+			float grad = 1.0f / -rightOffset;
+			return grad * (val - peakPoint) + 1.0f;
+		} else {
+			return 0.0f;
+		}
+		
+	}
+
+}
