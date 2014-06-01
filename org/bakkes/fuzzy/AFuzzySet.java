a652c42dcd93dec24ad72bee9bc70d3fb8aea18d
Jappie Klooster <jappieklooster@hotmail.com>
2014-06-01 15:09:20 +0200

    fuzzy set has now an equals

--- org/bakkes/fuzzy/AFuzzySet.java
+++ org/bakkes/fuzzy/AFuzzySet.java
@@ -16,4 +16,18 @@ public abstract class AFuzzySet extends AFuzzyTerm implements IFuzzyTerm{
 	public void setName(String name) {
 		this.name = name;
 	}
+	@Override
+	public boolean equals(Object to){
+		if(! (to instanceof AFuzzySet)){
+			return false;
+		}
+		AFuzzySet target = (AFuzzySet) to;
+		if(!target.getName().equals(getName())){
+			return false;
+		}
+		if(!(target.getValue() == getValue())){
+			return false;
+		}
+		return true;
+	}
 }
